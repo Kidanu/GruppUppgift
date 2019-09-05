73479f2bbdee6a0a2235b9a2b89ebe74af576573
Tobias Mindell <tobiasmindell@gmail.com>
2019-09-05 17:45:50 +0200

    AlltRest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try {
             JsonFile jsonfile = new JsonFile();
             jsonfile.createTemp();
             jsonfile.readTemp();
             JSONObject addingF = Soap.convertsCelsiusToFarenheit(jsonfile.readTemp()); //Tar ut Celelsius värdet ur arreyen och skickar till metoden.
-          //  System.out.println(addingF);
+            //System.out.println(addingF);
+            Rest rest = new Rest();
+            rest.sumForTempC(jsonfile.readTemp());
+            System.out.println("antal cel");
+
+
 
             try {
-                FileWriter file = new FileWriter("Nya_filen.json"); //Skapar en ny jsonfil med även Farenheit värden i.
+                FileWriter file = new FileWriter("Nya_filen.json", true); //Skapar en ny jsonfil med även Farenheit värden i.
                 file.write(addingF.toString());
+
                 file.close();
 
             } catch (IOException e) {
--- src/main/java/Rest.java
+++ src/main/java/Rest.java
@@ -1,2 +1,121 @@
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
 public class Rest {
+
+    public static void main(String[] args) throws IOException {
+
+}
+
+
+    int antalC = 0;
+    int antalF = 0;
+    int cValues = 0;
+    int fValues = 0;
+    String averageF = "";
+    String averageC = "";
+
+    /*public Rest(int antalC, int antalF, int cValues, int fValues) {
+        antalC = this.antalC;
+        antalF = this.antalF;
+        cValues = this.cValues;
+        fValues = this.fValues;
+    }*/
+
+    public void sumForTempC(JSONObject jsonObject) {//Räknar ut summan för all Celsius grader
+        JsonFile file = new JsonFile();
+        JSONArray array = jsonObject.getJSONArray("Temperatur");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject arrayObjekt = array.getJSONObject(i);
+            cValues += (Integer) arrayObjekt.get("C");
+            antalC = i;
+        }
+        System.out.println(antalC);
+    }
+
+    public void sumForTempF(JSONObject jsonObject) {                                //Räknar ut summan för alla farenheit grader
+        JSONArray array = jsonObject.getJSONArray("Temperatur");
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject arrayObjekt = array.getJSONObject(i);
+            fValues += (Integer) arrayObjekt.get("F");
+            antalF += i;
+        }
+    }
+
+    public void  averageTempMethodF () {                                            //Skickar rest api för att räkna ut medelvärdet på Farenheit
+        {
+            try {
+                URL url = new URL("https://api.mathjs.org/v4/?expr=" + this.fValues + "%2F" + this.antalF + "");
+                HttpsURLConnection con = null;
+                con = (HttpsURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                String result = "";
+                int responseCode = con.getResponseCode();
+                if (responseCode == 200) {
+                    Scanner scanner = new Scanner(con.getInputStream());
+                    while (scanner.hasNextLine()) {
+                        averageF += scanner.nextLine();
+                    }
+                    scanner.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(averageF);
+        }
+    }
+
+    public void averageTempMethodC() {                                                  //Skickar rest api för att räkna ut medevärden på Celsius
+        {
+            try {
+                URL url = new URL("https://api.mathjs.org/v4/?expr=" + this.cValues + "%2F" + this.antalC + "");
+                HttpsURLConnection con = null;
+                con = (HttpsURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                String result = "";
+                int responseCode = con.getResponseCode();
+                if (responseCode == 200) {
+                    Scanner scanner = new Scanner(con.getInputStream());
+                    while (scanner.hasNextLine()) {
+                        averageC += scanner.nextLine();
+                    }
+                    scanner.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public JSONObject averageTempJsonFile(){
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("Celsius medel", averageC);
+        jsonObject.put("Farenheit medel", averageF);
+        try {
+            FileWriter file = new FileWriter("AverageTemp.json");
+            file.write(jsonObject.toString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
 }
